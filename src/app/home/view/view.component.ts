07ce528dc1e135e2ef4efaa6079c0e82a543cda7
Birgit <birgit@Birgits-Air.lan>
2021-05-20 15:15:27 +0300

    model,pipes, translate

--- src/app/home/view/view.component.ts
+++ src/app/home/view/view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Item } from 'src/app/models/item.model';
 import { ItemService } from 'src/app/services/item.service';
 
 @Component({
@@ -8,7 +9,7 @@ import { ItemService } from 'src/app/services/item.service';
   styleUrls: ['./view.component.css']
 })
 export class ViewComponent implements OnInit {
-  item!: { title: string, price: number, imgSrc: string, category: string};
+  item!: Item;
 // Hüüumärgiga ei taha väärtust anda, annan väärtuse hiljem
 
 //ActivatedRouter on node_module sees asuv erline klass,
